6cfe2e57b8858ecb1f552676058bae98f10bd598
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2019-12-18 20:51:12 +0200

    38 (create_custom_directives 1)

--- src/main.js
+++ src/main.js
@@ -14,7 +14,20 @@ import "./filters"; //Global Filters
 
 Vue.config.productionTip = false;
 
+//Make Bold Custom Directive
+// Vue.directive('bold',{
+//     bind: function(el){
+//         el.style.fontWeight = "bold";
+//     }  
+// });
 
+
+//Make Dynamic Font Size Custom Directive
+Vue.directive('font',{
+    bind: function(el, binding){
+        el.style.fontSize = binding.value + 'px';
+    }  
+});
 
 new Vue({
     router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="about">
     <h1>{{pageName}}</h1>
-    <p>{{pageDescription}}</p>
+    <!-- <p v-bold>{{pageDescription}}</p> -->
+    <p v-font="60">{{pageDescription}}</p>
   </div>
 </template>
 
